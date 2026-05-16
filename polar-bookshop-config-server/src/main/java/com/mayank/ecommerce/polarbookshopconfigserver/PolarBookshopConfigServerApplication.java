42082c7edba58a7a225d7e5e17dccedc2685e772
Mayank Bhandari <mayank.bhandari@nyu.edu>
2026-05-16 23:40:09 +0530

    enabled config server

--- polar-bookshop-config-server/src/main/java/com/mayank/ecommerce/polarbookshopconfigserver/PolarBookshopConfigServerApplication.java
+++ polar-bookshop-config-server/src/main/java/com/mayank/ecommerce/polarbookshopconfigserver/PolarBookshopConfigServerApplication.java
@@ -2,8 +2,10 @@ package com.mayank.ecommerce.polarbookshopconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class PolarBookshopConfigServerApplication {
 
     public static void main(String[] args) {
