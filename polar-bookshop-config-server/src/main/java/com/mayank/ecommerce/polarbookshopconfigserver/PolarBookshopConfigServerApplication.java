133fe1cd86b402ca2a930678d1fa418fbc836a42
Mayank Bhandari <mayank.bhandari@nyu.edu>
2026-05-16 13:44:39 +0530

    Config server for Polar Bookshop: first commit

---
+++ polar-bookshop-config-server/src/main/java/com/mayank/ecommerce/polarbookshopconfigserver/PolarBookshopConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.mayank.ecommerce.polarbookshopconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolarBookshopConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PolarBookshopConfigServerApplication.class, args);
+    }
+
+}
---
+++ polar-bookshop-config-server/src/test/java/com/mayank/ecommerce/polarbookshopconfigserver/PolarBookshopConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mayank.ecommerce.polarbookshopconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolarBookshopConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
